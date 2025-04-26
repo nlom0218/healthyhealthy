0a3756fc5ce6e13e880568bb9bed828d8871556d
nlom0218 <nlom0218@naver.com>
2025-04-27 00:35:43 +0900

    feat: 팔굽 체크 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import { Button } from '@/components/ui/button';
+import { Checkbox } from '@/components/ui/checkbox';
 
 function App() {
   return (
-    <div>
-      Hello World <Button>Click me</Button>
+    <div className="my-0 mx-auto w-full lg:w-[900px]">
+      <div className="p-2">
+        {Array.from({ length: 5 }).map((_, index) => (
+          <div key={index} className="flex items-center gap-2 justify-between">
+            <span>팔굽혀펴기 20회 {index + 1}SET</span>
+            <Checkbox />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
